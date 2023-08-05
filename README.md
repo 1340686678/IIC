927941cd91d6f7474660af58a856ee1df5386564
1340686678 <46892453+1340686678@users.noreply.github.com>
2023-08-05 12:24:51 +0800

    V2.2
    跟新中断的读写封装

--- README.md
+++ README.md
@@ -8,18 +8,21 @@ H750
 参考：
 https://blog.csdn.net/qq_35697978/article/details/116375880?ops_request_misc=%257B%2522request%255Fid%2522%253A%2522169068449316800225544885%2522%252C%2522scm%2522%253A%252220140713.130102334..%2522%257D&request_id=169068449316800225544885&biz_id=0&utm_medium=distribute.pc_search_result.none-task-blog-2~all~sobaiduend~default-1-116375880-null-null.142^v91^control_2,239^v12^control&utm_term=STM32cube%2024c02&spm=1018.2226.3001.4187  
 **24C02参数**  
-总容量2K->char*32
+总容量2K 
 
 # demo1_1
-在HAL库的基础上继续封装  
+在HAL库的基础上继续封装   
 
 # demo1_2
-AT24C02通讯的读写都进行封装
+AT24C02通讯的读写都进行封装  
 
 # demo2
-使用I2C1 + 中断 与24C02通讯 
+使用I2C1 + 中断 与24C02通讯   
+
+# demo1_2
+使用I2C1 + 中断 与24C02通讯 的读写都进行封装  
 
 # demo3
 使用I2C1 + DMA 与24C02通讯
 **注意**
-使用DMA时，需要开启中断，不然发送完之后会一直占着总线。 
+使用DMA时，需要开启中断，不然发送完之后会一直占着总线。
---
+++ demo2_1/24C02Dev/24C02Dev.c
@@ -0,0 +1,168 @@
+#include "24C02Dev.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+HAL_StatusTypeDef AT24C02_Ready(void)
+{
+    return HAL_I2C_IsDeviceReady(&I2C_AT24C02,
+                                 ADDR_AT24C02_WRITE,
+                                 5,
+                                 100
+    );
+}
+
+HAL_StatusTypeDef AT24C02_write(uint16_t address, uint8_t data)
+{
+    HAL_StatusTypeDef status;
+    // status =  HAL_I2C_Mem_Write(   &I2C_AT24C02, 
+    //                                 ADDR_AT24C02_WRITE, 
+    //                                 address, 
+    //                                 I2C_MEMADD_SIZE_8BIT,
+    //                                 &data,
+    //                                 1, 
+    //                                 1000);
+    status = HAL_I2C_Mem_Write_IT(  &I2C_AT24C02,
+                                    ADDR_AT24C02_WRITE, 
+                                    address, 
+                                    I2C_MEMADD_SIZE_8BIT,
+                                    &data,
+                                    1);
+    //增加间隔防止 未准备好
+    HAL_Delay(10);
+    return  status;
+}
+
+
+HAL_StatusTypeDef AT24C02_write_Array(uint16_t address, uint8_t* data, uint16_t dataLen)
+{
+    HAL_StatusTypeDef status = HAL_OK;
+    for(int i = 0; i < dataLen; i++){
+        AT24C02_write(address + i, data[i]) ;
+    }
+
+    return status;
+}
+
+
+HAL_StatusTypeDef AT24C02_Read(uint16_t address, uint8_t *data)
+{
+    if(AT24C02_Ready() != HAL_OK)
+        return HAL_ERROR;
+    
+    HAL_StatusTypeDef status;
+    // status =  HAL_I2C_Mem_Read( &hi2c1, 
+    //                             ADDR_AT24C02_READ, 
+    //                             address, 
+    //                             I2C_MEMADD_SIZE_8BIT,
+    //                             data,
+    //                             1, 
+    //                             100);
+
+    status =  HAL_I2C_Mem_Read_IT( &hi2c1, 
+                                ADDR_AT24C02_READ, 
+                                address, 
+                                I2C_MEMADD_SIZE_8BIT,
+                                data,
+                                1);
+
+    //增加间隔防止 未准备好
+    HAL_Delay(10);
+    return  status;
+}
+
+HAL_StatusTypeDef AT24C02_Read_Array(uint16_t address,  uint8_t* data, uint16_t dataLen)
+{
+    if(AT24C02_Ready() != HAL_OK)
+        return HAL_ERROR;
+
+    HAL_StatusTypeDef status = HAL_OK;
+
+    for(int i = 0; i < dataLen; i++){
+        status = AT24C02_Read(address + i, data + i) ;
+    }
+
+    return status;
+}
+
+HAL_StatusTypeDef AT24C02_Inspect (void)
+{
+    printf("\r\nAT24C02 Inspect IT:\r\n");
+
+    if(AT24C02_Ready() != HAL_OK)
+    {
+        printf("I2C Dev ERROR\r\n");
+        return HAL_ERROR;
+    }
+    else{
+        printf("I2C Dev OK\r\n");
+
+    }
+
+    uint8_t I2C_Buffer_Write[256];
+    uint8_t I2C_Buffer_Read[256];
+    uint16_t i;
+    HAL_StatusTypeDef retStatus = HAL_OK;
+
+    /* WriteBuffer Initialization */
+    for(i=0; i<256; i++)
+    {
+        I2C_Buffer_Write[i] = i;    
+    }
+
+     /* write data to AT24C02 */
+    if(AT24C02_write_Array(0, I2C_Buffer_Write, sizeof(I2C_Buffer_Write)) == HAL_OK){
+        printf("Write OK\r\n");
+    }
+    else{
+        retStatus = HAL_ERROR;
+        printf("Write Failed\r\n");
+        goto RET;
+    }
+
+    /* read data from EEPROM */
+    printf("\r\nReading from AT24C02:\r\n");
+    AT24C02_Read_Array(0, I2C_Buffer_Read, 256);
+
+    // for(i=0; i<256; i++)
+    // {
+    //     printf("0x%02X  ",I2C_Buffer_Read[i]);
+    // }
+    // printf("\r\n");
+    // HAL_Delay(500);
+
+    /* check data */
+    if(memcmp(I2C_Buffer_Read,I2C_Buffer_Write,256) == 0 ) {
+        printf("Read OK\r\n");
+    }
+    else {
+        printf("Read Failed\r\n");
+        retStatus = HAL_ERROR;
+        goto RET;
+    }
+
+    /*clear write data*/
+    for(i=0; i<256; i++)
+    {
+        I2C_Buffer_Write[i]=0x00;    
+    }
+
+    AT24C02_write_Array(0, I2C_Buffer_Write, sizeof(I2C_Buffer_Write));
+
+    /*return status*/
+RET:
+    if(retStatus == HAL_OK)
+        printf("AT24C02 Work OK\r\n");
+    else
+        printf("AT24C02 Work ERROR\r\n");
+
+    return retStatus;
+}
+
+
+
+
+
+
+
